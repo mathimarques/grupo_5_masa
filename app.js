44855e8cb10c187ad737f72f93c855a97e8a72f9
SantiVB <SantiVB@MacBook-Pro-de-user.local>
2021-08-18 15:03:28 -0300

    setup basico para poder abrir la home, por ahora vacia, solo con titulo

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const path = require('path');
 const public = path.resolve(__dirname, "/public");
 app.use(express.static(public));
 
-app.listen('3030', ()=>{
-    console.log('Server initialized at http://localhost:3030');
+app.listen('3000', ()=>{
+    console.log('Server initialized at http://localhost:3000');
 })
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.resolve('./views/index.html'));
+});
