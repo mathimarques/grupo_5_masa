08f4fcd97f35eff2cb5f28a6fc19e5eb96feede7
A298769 <mamarques@santandertecnologia.com.ar>
2021-08-20 18:21:00 -0300

    Se agregan comenartarios y .gitignore de node_modules

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+// Definimos el path public como recurso de archivo estatico
 const public = path.resolve(__dirname, "/public");
 app.use(express.static(public));
 
+// Inicializamos el servidor
 app.listen('3000', ()=>{
     console.log('Server initialized at http://localhost:3000');
 })
 
+// Ruta raíz del proyecto
 app.get('/', (req,res)=>{
     res.sendFile(path.resolve('./views/index.html'));
 });
