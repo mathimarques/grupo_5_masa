5373cf4ba5bf9a763ca76675533b5baaf73ae64b
SantiVB <SantiVB@MBP-de-user.lan>
2021-08-25 15:28:07 -0300

    primeros bocetos de Home

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const public = path.resolve(__dirname, "/public");
+const public = path.resolve(__dirname, "./public");
 app.use(express.static(public));
 
 app.listen('3000', ()=>{
