f25a7b1fc77df011bfc9f2335974d4feeb128f89
A298769 <mamarques@santandertecnologia.com.ar>
2021-09-18 16:53:05 -0300

    Estructura de carpetas ejs

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 
 // Definimos el path public como recurso de archivo estatico
-const public = path.resolve(__dirname, "./public");
+const public = path.resolve(__dirname, "../public");
 app.use(express.static(public));
 
 // Inicializamos el servidor
@@ -13,27 +13,27 @@ app.listen('3000', ()=>{
 
 // Ruta raíz del proyecto
 app.get('/', (req,res)=>{
-    res.sendFile(path.resolve('./views/index.html'));
+    res.sendFile(path.resolve('./src/views/index.html'));
 });
 
 // Ruta raíz del login
 app.get('/login', (req,res)=>{
-    res.sendFile(path.resolve('./views/login.html'));
+    res.sendFile(path.resolve('./src/views/login.html'));
 });
 
 // Ruta raíz del register
 
 app.get('/register', (req, res) => {
-    res.sendFile(path.resolve('./views/register.html'));
+    res.sendFile(path.resolve('./src/views/register.html'));
 });
 
 // Ruta raíz del product
 app.get('/product', (req,res)=>{
-    res.sendFile(path.resolve('./views/product.html'));
+    res.sendFile(path.resolve('./src/views/product.html'));
 });
 
 // Ruta raíz del carrito
 
 app.get('/productCart', (req, res) => {
-    res.sendFile(path.resolve('./views/productCart.html'));
+    res.sendFile(path.resolve('./src/views/productCart.html'));
 });
