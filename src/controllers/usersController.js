bbd13b3b8d64ce0ec78b54e93e9883c6dc9d9850
santheartist <vazquezbare@gmail.com>
2021-10-18 15:25:28 -0300

    validacion login, muestra de errores

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,15 +1,59 @@
+// Requerimos modulos para trabajar con archivos
 const fs = require('fs');
 const path = require('path');
+
+// Requerimos modulo para validar errores en formulario de login
+const { validationResult } = require('express-validator');
+
+// Declaramos variables para trabajar con el archivo de usuarios
 const usersLocation = path.join(__dirname, '../data/users.json');
 const users = JSON.parse(fs.readFileSync(usersLocation, 'utf-8'));
 
+
 // Requerimos modulo para encriptar password
 const bcrypt = require('bcryptjs');
+const { off } = require('process');
 
 // Generamos el controlador con sus métodos
 const usersController = {
   login: (req, res) => {
     res.render("./users/login");
+  },
+  processLogin: (req,res) =>{
+      let errors = validationResult(req); //variable  para checkear si el middleware en el router detectó errores
+      let userToLog = undefined; //variable para luego asignar al usuario logueandose
+
+    // Logica para checkear si no hay errores, y si el usuario y contraseña en el 
+    // formulario (req.body) coinciden con algun usuario en nuestra base de datos (json por ahora)
+    // En caso que exista, se asigna el usuario a userToLog, caso contrario rebota
+      if(errors.isEmpty()){
+          for(let i=0; i<users.length; i++){
+              if(users[i].username == req.body.username){
+                  console.log('Username: ' + req.body.username + users[i].name);
+                  if(bcrypt.compareSync(req.body.password, users[i].password)){
+                      userToLog = users[i];
+                      break;
+                  }
+              }
+          }
+
+          if(userToLog==undefined){
+            res.render('./users/login', {errors: [
+                {msg: 'Hay errores de login'}
+                ]})
+            }
+  
+            res.send('username logueado: ' + userToLog.username + ' nombre de usuario: ' + userToLog.name);
+      }
+
+      else{
+          console.log(errors.mapped());
+          res.render('./users/login', {errors: errors.mapped(), old: req.body});
+      }
+
+      
+
+    //   res.redirect('/');
   },
   register: (req, res) => {
     res.render("./users/register");
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,11 +1,22 @@
 // Requerimos modulos
 const express = require('express');
 const usersController = require('../controllers/usersController');
-
+const {body, check} = require('express-validator');
 const router = express.Router();
 
-router.get('/login', usersController.login);
+// Middleware para validar login
+const validateLogin = [
+    check('username')
+        .notEmpty().withMessage('Completar nombre de usuario'),
+    check('password')
+        .notEmpty().withMessage('Completar el password') 
+];
 
+// Rutas para login
+router.get('/login', usersController.login);
+router.post('/login', validateLogin, usersController.processLogin);
+
+// Rutas para crear usuario
 router.get('/register', usersController.register);
 router.post('/register', usersController.processRegister);
 
