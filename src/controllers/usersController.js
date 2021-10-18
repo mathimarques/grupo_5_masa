832b0c5cb8dc80e5a81cdccc65fc782cc20f8cfd
santheartist <vazquezbare@gmail.com>
2021-10-18 11:01:58 -0300

    encriptado de password al creac usario

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 const usersLocation = path.join(__dirname, '../data/users.json');
-
 const users = JSON.parse(fs.readFileSync(usersLocation, 'utf-8'));
+
+// Requerimos modulo para encriptar password
+const bcrypt = require('bcryptjs');
 
 // Generamos el controlador con sus métodos
 const usersController = {
@@ -19,16 +21,13 @@ const usersController = {
 			username: req.body.username,
 			date: req.body.date,
 			address: req.body.address,
-            password: req.body.password
+            password: bcrypt.hashSync(req.body.password, 10)
 		}
 	users.push(newUsers);
 	fs.writeFileSync(usersLocation, JSON.stringify(users, null, " "))
     
     res.redirect("/");
-  },
-  productCart: (req, res) => {
-    res.render("productCart");
-  },
+  }
 };
 
 module.exports = usersController;
