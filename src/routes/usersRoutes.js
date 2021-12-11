3fe66d2a4da7fe1bd34f419123b0e625cb741713
mathimarques <mathias.marques@outlook.com>
2021-12-11 16:28:41 -0300

    validacion de upload grupal

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -78,6 +78,25 @@ const validateRegister = [
     .withMessage(
       "Debe Contener una letra mayúscula, una mínuscula, y un carácter especial"
     ),
+
+  check("userPic").custom((value, { req }) => {
+    let file = req.file;
+    let acceptedExtensions = [".jpg", ".png", "gif"];
+
+    if (!file) {
+      throw new Error("Tienes que subir una imágen");
+    } else {
+      let fileExtension = path.extname(file.originalname);
+      if (!acceptedExtensions.includes(fileExtension)) {
+        throw new Error(
+          `Las extensiones de archivos permitidas son ${acceptedExtensions.join(
+            ","
+          )}`
+        );
+      }
+    }
+    return true;
+  }),
 ];
 
 // Rutas para login
