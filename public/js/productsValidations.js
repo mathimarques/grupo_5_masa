917d081070e415d377f3f52a61b473de2b97e454
santheartist <vazquezbare@gmail.com>
2021-12-16 14:58:18 -0300

    valdacion frontend de file upload

--- public/js/productsValidations.js
+++ public/js/productsValidations.js
@@ -7,7 +7,10 @@ window.addEventListener('load', function(){
     let description = document.querySelector('#description');
     let popup2 = document.getElementById("myPopup2");
 
+    let upload_img = document.getElementById('upload_img');
+    let popup3 = document.getElementById("myPopup3");
 
+    // Validating model
     model.addEventListener('focusout', function(){
         if(model.value=="" || model.value.length<5){
             popup.classList.add("show");
@@ -19,6 +22,7 @@ window.addEventListener('load', function(){
         }
     });
 
+    // validating description
     description.addEventListener('focusout', function(){
         if(description.value=="" || description.value.length<20){
             popup2.classList.add("show");
@@ -30,4 +34,17 @@ window.addEventListener('load', function(){
         }
     });
 
+    // validating filetype
+    let allowedExtensions = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
+
+    upload_img.addEventListener('change', function(){
+        if(!allowedExtensions.exec(upload_img.value)){
+            popup3.classList.add("show");
+            fileInput.value = '';
+        }
+        else{
+            popup3.classList.remove("show");
+        }
+        
+    });
 });
